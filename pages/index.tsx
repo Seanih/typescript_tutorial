6d1597963aaaa886d96c36d577aa86fff641f2ad
Sean Anih <sean.anih@gmail.com>
2023-03-06 13:10:54 -0500

    added "delete" & "complete" functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
 import { v4 as uuidv4 } from 'uuid';
+import { AiOutlineCheck } from 'react-icons/ai';
 
 const Home: NextPage = () => {
 	const [task, setTask] = useState('');
@@ -15,7 +16,7 @@ const Home: NextPage = () => {
 		createdAt: Date;
 	}
 
-	const handleSubmit = (e: any) => {
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
 		const taskObj: Task = {
@@ -25,9 +26,37 @@ const Home: NextPage = () => {
 			createdAt: new Date(),
 		};
 
-		allTasks.push(taskObj);
+		setAllTasks([...allTasks, taskObj]);
+		sessionStorage.setItem('taskArray', JSON.stringify(allTasks));
 		setTask('');
 	};
+
+	const handleDelete = (taskName: string) => {
+		sessionStorage.removeItem(taskName);
+		const updatedTasks = allTasks.filter(task => task.title !== taskName);
+		setAllTasks(updatedTasks);
+	};
+
+	const handleComplete = (taskName: string) => {
+		allTasks.filter(item => {
+			if (taskName === item.title) {
+				item.completed = true;
+
+				console.log(item);
+			}
+		});
+
+		setAllTasks([...allTasks]);
+	};
+
+	useEffect(() => {
+		const storedData = localStorage.getItem('taskArray');
+		if (storedData) {
+			setAllTasks(JSON.parse(storedData));
+		}
+
+		console.log(allTasks);
+	}, [allTasks]);
 
 	return (
 		<div className={styles.container}>
@@ -39,7 +68,7 @@ const Home: NextPage = () => {
 
 			<div className='flex flex-col items-center'>
 				<h1 className='my-8'>Typescript ToDo App</h1>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<label className='block' htmlFor='task_input'>
 						Add Task:
 					</label>
@@ -54,9 +83,8 @@ const Home: NextPage = () => {
 					<button
 						className='border rounded-md h-8 p-1 ml-2 hover:cursor-pointer hover:bg-slate-300 hover:text-black'
 						type='submit'
-						onClick={handleSubmit}
 					>
-						ADD TASK
+						ADD
 					</button>
 				</form>
 				<section>
@@ -64,14 +92,24 @@ const Home: NextPage = () => {
 						{allTasks.map(item => (
 							<li
 								key={item.id}
-								className='relative border rounded-md mb-4 px-4 py-2 text-center'
+								className={`relative border rounded-md mb-4 px-4 py-2 text-center ${
+									item.completed ? 'line-through' : ''
+								}`}
 							>
 								<div>{item.title}</div>
 								<button
 									className='absolute border top-0 left-[-3rem] h-full w-8 flex justify-center items-center bg-red-400 hover:bg-red-600'
 									type='button'
+									onClick={() => handleDelete(item.title)}
 								>
 									x
+								</button>
+								<button
+									className='absolute border top-0 right-[-3rem] h-full w-8 flex justify-center items-center bg-green-400 hover:bg-green-600'
+									type='button'
+									onClick={() => handleComplete(item.title)}
+								>
+									<AiOutlineCheck />
 								</button>
 							</li>
 						))}
